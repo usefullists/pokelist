3f1bc4a8d47c177da504d703fd55ad212101cf6a
usefullists <>
2022-05-17 14:23:04 +0200

    removed popular

--- src/lib/header/header.svelte
+++ src/lib/header/header.svelte
@@ -87,14 +87,6 @@
 							List
 						</a>
 						<a
-							href="/popular"
-							class:active={$page.url.pathname === '/popular'}
-							class="hover:text-indigo-600 dark:text-indigo-200 dark:hover:text-indigo-300 text-base font-medium"
-							sveltekit:prefetch
-						>
-							Popular
-						</a>
-						<a
 							href="/about"
 							class:active={$page.url.pathname === '/about'}
 							class="hover:text-indigo-600 dark:text-indigo-200 dark:hover:text-indigo-300 text-base font-medium"
@@ -252,31 +244,6 @@
 								</svg>
 								<span class="dark:text-indigo-200 ml-3 text-base font-medium text-gray-900">
 									List
-								</span>
-							</a>
-
-							<a
-								href="/popular"
-								class:active={$page.url.pathname === '/popular'}
-								class="hover:bg-gray-50 dark:hover:bg-indigo-600 flex items-center p-3 -m-3 rounded-md"
-								sveltekit:prefetch
-							>
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									class="dark:text-indigo-400 flex-shrink-0 w-6 h-6 text-indigo-600"
-									fill="none"
-									viewBox="0 0 24 24"
-									stroke="currentColor"
-									stroke-width="2"
-								>
-									<path
-										stroke-linecap="round"
-										stroke-linejoin="round"
-										d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
-									/>
-								</svg>
-								<span class="dark:text-indigo-200 ml-3 text-base font-medium text-gray-900">
-									Popular
 								</span>
 							</a>
 
