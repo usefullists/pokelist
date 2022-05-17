1ba198dcac12311acc7dadceb775d4758890fd1a
usefullists <>
2022-05-17 14:33:20 +0200

    limit query

--- src/routes/list.svelte
+++ src/routes/list.svelte
@@ -1,7 +1,7 @@
 
 <script context="module">
 	export const load = async ({ fetch }) => {
-		const pokemons = await fetch('./pokeapi.json?limit=100000&offset=0').then((response => response.json()))
+		const pokemons = await fetch('./pokeapi.json?limit=400').then((response => response.json()))
 		return {
 			props: {
 				pokemons: pokemons.pokemons
